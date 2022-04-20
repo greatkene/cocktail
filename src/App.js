b3c6bdd84427177f89108837ad6dde5e047a9693
Great <ihevuemeg@gmail.com>
2022-04-20 12:33:07 +0100

    router set

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-export default function App() {
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Import Pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Cocktail from './pages/Cocktail'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+import Contact from './pages/Contact'
+
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/cocktail' element={<Cocktail/>}/>
+        <Route path='/cocktail/:id' element={<SingleCocktail/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
