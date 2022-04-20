19783c7648b17ebc4740b23cc9e60fa5a3b813c7
Great <ihevuemeg@gmail.com>
2022-04-21 00:15:53 +0100

    fixed nav

--- src/components/Attract.js
+++ src/components/Attract.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Attract = () => {
   return (
-    <div id='attract'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nihil eos quisquam non, fugit quibusdam quo labore neque cumque corrupti, vel officia, ad dicta nam? Eos accusamus similique maiores dicta ipsam vero consectetur, modi doloremque eveniet ducimus odio libero optio ullam maxime possimus aliquid voluptas corrupti architecto sed sapiente odit quasi, saepe recusandae consequuntur? Repudiandae molestiae, facilis debitis sed in id excepturi et! Repudiandae adipisci quia dolore culpa corporis, inventore veritatis vero provident libero harum modi necessitatibus, pariatur, expedita voluptas non? Doloremque voluptatum a voluptates vitae? Culpa recusandae aspernatur rerum consequatur officia ex repudiandae, omnis, voluptatem, ipsum quibusdam debitis velit?
+    <div className='mt-20' id='attract'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nihil eos quisquam non, fugit quibusdam quo labore neque cumque corrupti, vel officia, ad dicta nam? Eos accusamus similique maiores dicta ipsam vero consectetur, modi doloremque eveniet ducimus odio libero optio ullam maxime possimus aliquid voluptas corrupti architecto sed sapiente odit quasi, saepe recusandae consequuntur? Repudiandae molestiae, facilis debitis sed in id excepturi et! Repudiandae adipisci quia dolore culpa corporis, inventore veritatis vero provident libero harum modi necessitatibus, pariatur, expedita voluptas non? Doloremque voluptatum a voluptates vitae? Culpa recusandae aspernatur rerum consequatur officia ex repudiandae, omnis, voluptatem, ipsum quibusdam debitis velit?
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import logo from '../assets/logo.png'
 import {Link as Link1} from 'react-scroll'
 import { Link as Link2 } from 'react-router-dom'
@@ -9,10 +9,27 @@ import {FiMoreVertical} from 'react-icons/fi'
 const Navbar = () => {
     const [openMobile, setOpenMobile] = useState(false)
 
+    // Handling Background change on scroll
+    const [navBackground, setNavBackground] = useState(false)
+    const navRef = useRef()
+    navRef.current = navBackground
+    useEffect(() => {
+        const handleScroll = () => {
+            const show = window.screenY >= 40
+            if (navRef.current !== show) {
+                setNavBackground(show)
+            }
+        }
+        document.addEventListener('scroll', handleScroll)
+        return () => {
+            document.removeEventListener('scroll', handleScroll)
+        }
+    })
+
   return (
-    <nav className='flex justify-between py-4 md:px-24 px-4'>
+    <nav className={navBackground ? 'navbar active' : 'navbar'} role='navigation'>
         <img src={logo} alt="logo" className='h-16 cursor-pointer' />
-        <ul className='font-medium text-lg md:flex hidden items-center gap-8 cursor-pointer'>
+        <ul className='font-medium text-white text-lg md:flex hidden items-center gap-8 cursor-pointer'>
             <li>
                 <Link1 to="/" spy={true} smooth={true}>Home</Link1>
             </li>
@@ -37,10 +54,10 @@ const Navbar = () => {
         </ul>
 
         {/*Mobile Nav*/}
-        <ul className={`fixed top-0 z-50 bg-white w-2/4 h-screen shadow-2xl md:hidden
+        <ul className={`fixed top-0 z-50 text-black bg-white w-2/4 h-screen shadow-2xl md:hidden
         flex flex-col gap-10 p-7 pt-20 duration-500 ${openMobile ? 'right-0' : 'right-[-100%]'}`}>
             <li>
-                <Link1 to="/" spy={true} smooth={true}>Home</Link1>
+                <Link2 to="/">Home</Link2>
             </li>
             <li>
                 <Link2 to="/about">About</Link2>
@@ -62,9 +79,9 @@ const Navbar = () => {
             </div>
         </ul>
 
-        <div className="md:hidden my-4 text-2xl z-50 cursor-pointer" onClick={()=>setOpenMobile(!openMobile)}>
+        <div className="md:hidden text-white my-4 text-2xl z-50 cursor-pointer" onClick={()=>setOpenMobile(!openMobile)}>
             {
-                openMobile ? <FaTimes/> : <FaBars/>
+                openMobile ? <FaTimes className='text-black'/> : <FaBars/>
             }
         </div>
         
