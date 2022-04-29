9200497fe776dfb0169355e3234d1452e9ff549f
Great <ihevuemeg@gmail.com>
2022-04-29 11:14:22 +0100

    search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div className="flex items-center justify-center py-10">
+        <div className="flex border-2 border-gray-200 rounded">
+            <input type="text" className="px-4 py-2 w-80"  placeholder="Search..." />
+            <button className="px-4 text-white bg-gray-600 border-l">
+                Search
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default SearchBar
--- src/pages/Cocktail.js
+++ src/pages/Cocktail.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
+import SearchBar from '../components/SearchBar'
 
 const Cocktail = () => {
   return (
     <section>
       <BreadCrumb>Cocktail</BreadCrumb>
+      <SearchBar/>
     </section>
   )
 }
